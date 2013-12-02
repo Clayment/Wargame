f1a429673ae2a8224ab53e827493bc4f3ea827c8
Clayment <clemhorgues@free.fr>
2013-12-02 19:25:24 +0100

    Gen Map : Nombre de noeuds en cours

--- src/wargame/IConfig.java
+++ src/wargame/IConfig.java
@@ -3,10 +3,14 @@ package wargame;
 import java.awt.Color;
  
 public interface IConfig {
-    int LARGEUR_CARTE = 40; int HAUTEUR_CARTE = 15;                         // en nombre de cases
+    int LARGEUR_CARTE = 25; int HAUTEUR_CARTE = 15;                         // en nombre de cases
     int NB_PIX_CASE = 40;
     int POSITION_X = 100; int POSITION_Y = 50;                              // Position de la fenêtre
     int NB_HEROS = 15; int NB_MONSTRES = 30; int NB_OBSTACLES = 100;
+    
+    int FREQ_WATER = 100; int FREQ_MOUNTAIN = 50; int FREQ_FOREST = 40;
+    int MIN_WATER = 1; int MIN_MOUNTAIN = 2; int MIN_FOREST = 3;
+    
     Color COULEUR_NEUTRE = Color.magenta, COULEUR_TEXTE = Color.black;
     Color COULEUR_HEROS = Color.red, COULEUR_HEROS_OFF = Color.pink;
     Color COULEUR_MONSTRES = Color.black;
